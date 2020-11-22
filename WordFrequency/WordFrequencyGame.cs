dcb6fcbc4418c0de5708a3b19c2ad95f6816c383
Shishi Zhang <SZhang42@slb.com>
2020-11-22 11:48:39 +0800

    refactor: comment: delete the useless comment

--- WordFrequency/WordFrequencyGame.cs
+++ WordFrequency/WordFrequencyGame.cs
@@ -34,7 +34,6 @@ namespace WordFrequency
 
             List<string> strList = new List<string>();
 
-            //stringJoiner joiner = new stringJoiner("\n");
             foreach (Input w in inputList)
             {
                 string s = w.Value + " " + w.WordCount;
@@ -49,7 +48,6 @@ namespace WordFrequency
             Dictionary<string, List<Input>> map = new Dictionary<string, List<Input>>();
             foreach (var input in inputList)
             {
-                //       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
                 if (!map.ContainsKey(input.Value))
                 {
                     List<Input> arr = new List<Input>();
